aae3f0596ee959e92150667139f8e45b71cc3d2a
何乐 <work@imhele.com>
2020-12-28 23:20:33 +0800

    Deploy in Aliyun OSS (#1)
    * Config build actions
    
    * Revert: Change build branch
    
    * Adjust font style for Chinese
    
    * Upload to Aliyun OSS
    
    * Upload to Aliyun OSS as public read file

---
+++ deploy-oss.py
@@ -0,0 +1,18 @@
+import os
+import oss2
+
+AUTH = oss2.Auth(os.environ.get('AccessKeyId'), os.environ.get('AccessKeySecret'))
+BUCKET = oss2.Bucket(AUTH, os.environ.get('EndPoint'), os.environ.get('BucketName'))
+
+ROOT_DIR = os.path.join(os.getcwd(), 'output')
+PATH_LIST = [ROOT_DIR]
+PUT_OBJECT_HEADERS = {
+  'x-oss-object-acl': oss2.OBJECT_ACL_PUBLIC_READ
+}
+
+while len(PATH_LIST):
+  p = PATH_LIST.pop()
+  if os.path.isdir(p):
+    PATH_LIST += map(lambda i: os.path.join(p, i), os.listdir(p))
+  elif os.path.isfile(p):
+    BUCKET.put_object_from_file(os.path.relpath(p, ROOT_DIR), p, headers=PUT_OBJECT_HEADERS)
