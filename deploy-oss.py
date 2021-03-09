7e1319083f71ecea34760609595c870064f29330
何乐 <work@imhele.com>
2021-03-09 18:51:55 +0800

    Imhele/perf/resources (#9)
    * Use mirror resources instead
    
    * Skip upload if the md5 of file does not change

--- deploy-oss.py
+++ deploy-oss.py
@@ -1,20 +1,61 @@
+from base64 import b64encode
+from functools import partial
+from hashlib import md5
+from http import CaseInsensitiveDict
 import os
 import oss2
 
-AUTH = oss2.Auth(os.environ.get('AccessKeyId'), os.environ.get('AccessKeySecret'))
-BUCKET = oss2.Bucket(AUTH, os.environ.get('EndPoint'), os.environ.get('BucketName'))
 
-ROOT_DIR = os.path.join(os.getcwd(), 'output')
-PATH_LIST = [ROOT_DIR]
-PUT_OBJECT_HEADERS = {
-  'x-oss-object-acl': oss2.OBJECT_ACL_PUBLIC_READ
-}
+class Uploader:
+    __output_path: str
 
-while len(PATH_LIST):
-  p = PATH_LIST.pop()
-  if os.path.isdir(p):
-    PATH_LIST += map(lambda i: os.path.join(p, i), os.listdir(p))
-  elif os.path.isfile(p):
-    target = os.path.relpath(p, ROOT_DIR)
-    print('Uploading %s... (%d files / dirs rest)' % (target, len(PATH_LIST)))
-    BUCKET.put_object_from_file(target, p, headers=PUT_OBJECT_HEADERS)
+    bucket: oss2.Bucket
+    put_object_headers: CaseInsensitiveDict = CaseInsensitiveDict({
+        'x-oss-object-acl': oss2.OBJECT_ACL_PUBLIC_READ
+    })
+
+    def __init__(self, bucket: oss2.Bucket, output_path: str):
+        self.bucket = bucket
+        self.__output_path = os.path.abspath(output_path)
+
+    def start(self) -> None:
+        for dirname, dirnames, filenames in os.walk(self.__output_path):
+            ossdir = os.path.relpath(dirname, self.__output_path)
+            for filename in filenames:
+                osskey = os.path.join(ossdir, filename)
+                filepath = os.path.join(dirname, filename)
+                if self.__should_skip_upload(filepath, osskey):
+                    print('Skip upload %s' % osskey)
+                    continue
+                print('Uploading %s...' % osskey)
+                self.bucket.put_object_from_file(
+                    osskey, filepath, headers=self.put_object_headers)
+
+    def __calc_local_md5(self, filepath: str) -> str:
+        md5_hash = md5()
+        with open(filepath, mode='rb') as f:
+            for buffer in iter(partial(f.read, 128), b''):
+                md5_hash.update(buffer)
+        return b64encode(md5_hash.digest()).decode()
+
+    def __fetch_remote_md5(self, osskey: str) -> str:
+        try:
+            return self.bucket.head_object(osskey).headers['content-md5']
+        except:
+            return ''
+
+    def __should_skip_upload(self, filepath: str, osskey: str) -> bool:
+        remote_md5 = self.__fetch_remote_md5(osskey)
+        return bool(remote_md5 and remote_md5 == self.__calc_local_md5(filepath))
+
+
+if __name__ == '__main__':
+    EndPoint = os.environ.get('EndPoint')
+    BucketName = os.environ.get('BucketName')
+    AccessKeyId = os.environ.get('AccessKeyId')
+    AccessKeySecret = os.environ.get('AccessKeySecret')
+
+    auth = oss2.AuthV2(AccessKeyId, AccessKeySecret)
+    bucket = oss2.Bucket(auth, EndPoint, BucketName)
+
+    Uploader(bucket, 'output').start()
